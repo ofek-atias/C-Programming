c771fc6e96bdb1ca04d80158ee00a411ba6ea2ef
ofek-atias <ofeqi123@gmail.com>
2025-12-03 16:01:53 +0200

    ex2 almost final

--- ex2.c
+++ ex2.c
@@ -18,30 +18,81 @@ void lower_string(char str[MAX_STRING_LEN])
     }
 }
 
+void single_char_cycle(char str[MAX_STRING_LEN])
+{
+    char first_char = str[0];
+    int i = 0;
+
+    while(str[i+1] != '\0')
+    {
+        str[i] = str[i+1];
+        i++;
+    }
+    str[i] = first_char;
+}
+
+
+
+void cycliced_str(char rotated_str[MAX_STRING_LEN][MAX_STRING_LEN], char str[MAX_STRING_LEN])
+{
+    int i = 0;
+    int length = strlen(str);
+    char first_char;
+    char current_str[MAX_STRING_LEN] = "";
+
+    strcpy(rotated_str[0], str);
+
+    strcpy(current_str, str);
+
+    for (i = 1; i < length; i++)
+    {
+        single_char_cycle(current_str);
+        strcpy(rotated_str[i], current_str);
+    }
+}
+
+
 int main() {
 
-    char str[MAX_STRING_LEN];
+    char str[MAX_STRING_LEN] = "";
+    char original_str[MAX_STRING_LEN] = "";
     char str_pool[MAX_STRING_POOL_SIZE][MAX_STRING_LEN];
+    char rotated_str[MAX_STRING_LEN][MAX_STRING_LEN];
 
-    int counter = 0;
-    int pool_len = 0;
+    int pool_counter = 0;
+    int pool_len = sizeof(str_pool);
+    int match_counter = 0;
 
-    memset(str_pool, 0 , sizeof(str_pool)); //Initialize the pool to be each slot empty
+    memset(str_pool, 0 , sizeof(pool_len)); //Initialize the pool to be each slot empty
 
     printf("Enter the search string:\n");
     scanf("%10s", str);
 
+    strcpy(original_str, str);
+
     lower_string(str);
+    cycliced_str(rotated_str,str);
     //printf("%s\n",str);
 
 
     printf("Enter the strings pool:\n");
 
-    while ((fgets(str_pool[counter], MAX_STRING_POOL_SIZE , stdin) != NULL) &&  counter < 20)
+    while ((scanf("%10s",str_pool[pool_counter]) != -1) &&  pool_counter < 20)
     {
-        lower_string(str_pool[counter]);
-        //printf("%s", str_pool[counter]);
-        counter++;
+        lower_string(str_pool[pool_counter]);
+        pool_counter++;
     }
+
+    for(int i = 0; i < strlen(str);i++)
+    {
+        for (int j = 0; j < pool_counter; j++)
+        {
+            int cmp = strcmp(rotated_str[i],str_pool[j]);
+            if (cmp==0)
+                match_counter++;
+        }
+    } 
+    printf("Number of rotations of \"%s\" in the strings pool is: %d\n", original_str, match_counter);
+
     return 0;
 }
