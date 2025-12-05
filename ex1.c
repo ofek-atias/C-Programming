ab88986efea233df1fe50b7894026687517ed7fe
lidoratias <lidor1atias@gmail.com>
2025-12-05 16:27:35 +0200

    Fixed multiple logic and runtime issues in EX1 (tic-tac-toe).

--- ex1.c
+++ ex1.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
 
+#define MAX_BOARD_SIZE 10
+#define PLAYER_ONE 1
+#define PLAYER_TWO 2
 
-void player_choice(int name,char board[10][10],int size);
-void board_print(char board[10][10],int size);
-int win_check(int name, char board[10][10], int size);
-int tie_check(char board[10][10], int size);
+#define FALSE 0
+#define TRUE 1
 
+#define VALID_CELL_VALUE 1
+#define OUT_OF_BOUNDS_ERR_CODE -1
+#define OCCUPIED_CELL_ERR_CODE -2
 
-void player_choice(int name,char board[10][10],int size)
+void player_choice(int name,char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE],int size);
+void board_print(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE],int size);
+int win_check(int name, char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size);
+int tie_check(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size);
+int check_cell_validity(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int pos_row, int pos_col, int size);
+
+int check_cell_validity(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int pos_row, int pos_col, int size) {
+    if ((pos_row < 1) || (pos_row > size) || (pos_col < 1) || (pos_col > size)) {
+        return OUT_OF_BOUNDS_ERR_CODE;
+    }
+
+    if ((board[pos_row-1][pos_col-1] == 'X') || (board[pos_row-1][pos_col-1] == 'O')) {
+        return OCCUPIED_CELL_ERR_CODE;
+    }
+
+    return VALID_CELL_VALUE;
+}
+
+void player_choice(int name, char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size)
 {
-    int pos_row = -1, pos_col = -1;
-    printf("Player %d, please insert your move:\n",name);
-    scanf("%d,%d",&pos_row,&pos_col);
-    while((pos_row < 1) || (pos_row > size) || (pos_col < 1) || (pos_col > size))
-        {
+    int pos_row = -1, pos_col = -1, validity_indicator = VALID_CELL_VALUE;
+    printf("Player %d, please insert your move:\n", name);
+    scanf("%d,%d", &pos_row, &pos_col);
+    
+    validity_indicator = check_cell_validity(board, pos_row, pos_col, size);
+    while (validity_indicator == OUT_OF_BOUNDS_ERR_CODE || validity_indicator == OCCUPIED_CELL_ERR_CODE) {
+        if (validity_indicator == OUT_OF_BOUNDS_ERR_CODE) {
             printf("Invalid indices (out of bounds), please choose your move again:\n");
-             scanf("%d,%d",&pos_row,&pos_col);
         }
-    while((board[pos_row-1][pos_col-1] == 'X') || (board[pos_row-1][pos_col-1] == 'O') )
-        {
+        else if (validity_indicator == OCCUPIED_CELL_ERR_CODE) {
             printf("Invalid indices (occupied cell), please choose your move again:\n");
-             scanf("%d,%d",&pos_row,&pos_col);
         }
+        
+        scanf("%d,%d",&pos_row,&pos_col);
+        validity_indicator = check_cell_validity(board, pos_row, pos_col, size);
+    }
+
     switch (name)
     {
         case 1:
@@ -30,51 +56,44 @@ void player_choice(int name,char board[10][10],int size)
          case 2:
             board[pos_row-1][pos_col-1] = 'O';
             break;
-    } 
+    }
     board_print(board, size);
-
 }
-void board_print(char board[10][10],int size)
+void board_print(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size)
 {
-    for (int i = 0;i < size;i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0;j < size;j++)
+        for (int j = 0; j < size; j++)
         {
-            printf("%c ",board[i][j]);
+            if (j == size - 1)
+                printf("%c", board[i][j]);
+            else
+                printf("%c ", board[i][j]);
         }
         printf("\n");
     }
 }
 
-int win_check(int name, char board[10][10], int size)
+int win_check(int name, char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size)
 {
     int count_row = 0;
     int count_col = 0;
     int count_diag = 0;
     int count_anti_diag = 0;
+
+    char player_char = (name == PLAYER_ONE) ? 'X' : 'O';
+
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            if (name == 1)
-            {
-                if (board[i][j] == 'X')
-                    count_row +=1;
-                if (board[j][i] == 'X')
-                    count_col +=1;
-            }
-            else 
-            {
-                if (board[i][j] == 'O')
-                    count_row +=1;
-                if (board[j][i] == 'O')
-                    count_col +=1;
-            }
-
-            
+            if (board[i][j] == player_char)
+                count_row +=1;
+            if (board[j][i] == player_char)
+                count_col +=1;
         }
         if (count_row == size || count_col == size)
-            return name;
+            return TRUE;
         count_row = 0;
         count_col = 0;
 
@@ -82,31 +101,18 @@ int win_check(int name, char board[10][10], int size)
    
     for (int i = 0; i < size; i++)
     {
-        if (name == 1)
-        {
-            if (board[i][i] == 'X')
-                count_diag +=1;
-            if (board[i][size-1-i] == 'X')
-                count_anti_diag +=1;
-            
-        }
-        else 
-        {
-            if (board[i][i] == 'O')
-                count_diag +=1;
-            if (board[i][size-1-i] == 'O')
-                count_anti_diag +=1;
-        }
+        if (board[i][i] == player_char)
+            count_diag +=1;
+        if (board[i][size-1-i] == player_char)
+            count_anti_diag +=1;
     }
     if (count_diag == size || count_anti_diag == size)
-            return name;
+            return TRUE;
     
-    return 0;
-
-
+    return FALSE;
 }
 
-int tie_check(char board[10][10], int size)
+int tie_check(char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE], int size)
 {
     int count = 0;
 
@@ -137,16 +143,15 @@ int tie_check(char board[10][10], int size)
                 count_o_col +=1;
         }
         if (count_x_row > 0 && count_o_row > 0)
-            count ++;
+            count++;
         
         if (count_x_col > 0 && count_o_col > 0)
-            count ++;
+            count++;
             
         count_x_row = 0;
         count_o_row = 0;
         count_x_col = 0;
         count_o_col = 0;
-
     }
    
     for (int i = 0; i < size; i++)
@@ -162,53 +167,55 @@ int tie_check(char board[10][10], int size)
             count_o_anti_diag +=1;
     }
     if (count_x_diag > 0 && count_o_diag > 0)
-        count ++;
+        count++;
         
     if (count_x_anti_diag > 0 && count_o_anti_diag > 0)
-        count ++;   
+        count++;   
 
-    if (count == size*2+2)
-            return 1;
+    if (count == size*2 + 2)
+            return TRUE;
     
-    return 0;
-
-
+    return FALSE;
 }
 
 
 int main()
 {
-    char board[10][10];
+    char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
     int size = 0;
-    int current_player = 1;
+    int current_player = PLAYER_ONE;
 
     printf("Please enter the board size N [1-10]:\n");
-    scanf("%d",&size);
-    printf("Welcome to %dx%d Tic-Tac-Toe:\n",size,size);
-    for (int i = 0;i < size;i++)
+    scanf("%d", &size);
+    printf("Welcome to %dx%d Tic-Tac-Toe:\n", size, size);
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0;j < size;j++)
+        for (int j = 0; j < size; j++)
         {
             board[i][j] = '_';
-            printf("%c ",board[i][j]);
+
+            if (j == size - 1)
+                printf("%c", board[i][j]);
+            else
+                printf("%c ", board[i][j]);
         }
         printf("\n");
     }
-    while (win_check(1,board,size) == 0 && win_check(2,board,size) == 0 && tie_check(board,size) == 0)
+    while (win_check(PLAYER_ONE, board, size) == FALSE && win_check(PLAYER_TWO, board, size) == FALSE && tie_check(board, size) == FALSE)
     {
-        if (current_player % 2 == 1)
-            player_choice(1,board,size);
+        if (current_player % 2 == PLAYER_ONE)
+            player_choice(PLAYER_ONE, board, size);
         else
-            player_choice(2,board,size);
-        current_player = (current_player%2) + 1;
+            player_choice(PLAYER_TWO, board, size);
+        current_player = (current_player % 2) + 1;
     }
-    if (win_check(1,board,size) == 1)
-        printf("Player 1 is the winner!\n");
-    else if (win_check(2,board,size) == 2)
-        printf("Player 2 is the winner!\n");
-    else if (tie_check(board,size) ==1)
-        printf("There is a Tie!\n");
 
+    if (win_check(PLAYER_ONE, board, size) == TRUE)
+        printf("Player 1 is the winner!\n");
+    else if (win_check(PLAYER_TWO, board, size) == TRUE)
+        printf("Player 2 is the winner!\n");
+    else if (tie_check(board, size) == TRUE)
+        printf("There is a Tie!\n");
 
     return 0;
 }
